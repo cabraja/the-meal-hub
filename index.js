1092ccf4e96f9302b4364f7795f92b81ce222bfc
cabraja <cabrajamihajlo@gmail.com>
2021-09-04 00:11:35 +0200

    added categories

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
 
 const RAN_MEAL_API = 'https://www.themealdb.com/api/json/v1/1/random.php';
+const CATEGORY_API = 'https://www.themealdb.com/api/json/v1/1/categories.php';
 const mealsSuggested = document.querySelectorAll('.meal');
 
+// GET DATA ---------
 const getSuggested = (url) => {
      mealsSuggested.forEach((meal,index) => {
          fetch(url).then(res => res.json()).then(data => showSuggested(data.meals[0],index));
      })
 }
+    const getCagetories = (url) => {
+        fetch(url).then(res => res.json()).then(data => showCategories(data.categories));
+    }
+
+
+
+// SHOW DATA ----------
+
+const categoryContainer = document.getElementById('categories-container');
 
 const showSuggested = (data,index) => {
-    mealsSuggested[index].innerHTML=`<img src=${data.strMealThumb} >`;
+    mealsSuggested[index].innerHTML=`
+        <h4>${data.strMeal}</h4>
+        <img src=${data.strMealThumb} />
+    `;
 }
 
+const showCategories = (data) => {
+    data.forEach(category => {
+        const element = document.createElement('div');
+        element.classList.add('category');
+        element.innerHTML = `
+            <h3>${category.strCategory}</h3>
+        `;
+        categoryContainer.appendChild(element);
+    })
+}
+
+
+
+
+// FUNCTION CALLS -----------------
 getSuggested(RAN_MEAL_API);
+getCagetories(CATEGORY_API);
