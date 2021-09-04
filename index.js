f2b1c895cc285f721d5460ba3a1618df73374539
cabraja <cabrajamihajlo@gmail.com>
2021-09-05 00:35:38 +0200

    added category results

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const RAN_MEAL_API = 'https://www.themealdb.com/api/json/v1/1/random.php';
 const CATEGORY_API = 'https://www.themealdb.com/api/json/v1/1/categories.php';
+const FILTER_API = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=';
 const mealsSuggested = document.querySelectorAll('.meal');
 
 // GET DATA ---------
@@ -9,9 +10,17 @@ const getSuggested = (url) => {
          fetch(url).then(res => res.json()).then(data => showSuggested(data.meals[0],index));
      })
 }
-    const getCagetories = (url) => {
-        fetch(url).then(res => res.json()).then(data => showCategories(data.categories));
-    }
+
+const getCagetories = (url) => {
+    fetch(url).then(res => res.json()).then(data => showCategories(data.categories));
+}
+
+const getFiltered = (e) => {
+    const main = document.getElementById('main');
+    main.innerHTML = 'Loading...';
+    const criteria = e.target.innerHTML;
+    fetch(FILTER_API + criteria).then(res => res.json()).then(data => showFiltered(data.meals));
+}
 
 
 
@@ -42,10 +51,29 @@ const showCategories = (data) => {
         const element = document.createElement('div');
         element.classList.add('category');
         element.innerHTML = `
-            <h3>${category.strCategory}</h3>
+            <h3 onclick="getFiltered(event)">${category.strCategory}</h3>
         `;
         categoryContainer.appendChild(element);
+    
     })
+}
+
+const showFiltered = (data) => {
+    const main = document.getElementById('main');
+    main.innerHTML = '';
+
+    data.forEach(meal => {
+        const element = document.createElement('div');
+        element.classList.add('meal-main');
+        element.innerHTML=`
+        <h4>${meal.strMeal}</h4>
+        <div class="meal-main-image" style="background-image:url(${meal.strMealThumb});"></div>
+    `;
+
+    main.appendChild(element);
+    })
+
+    
 }
 
 
@@ -54,3 +82,4 @@ const showCategories = (data) => {
 // FUNCTION CALLS -----------------
 getSuggested(RAN_MEAL_API);
 getCagetories(CATEGORY_API);
+
